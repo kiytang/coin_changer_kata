28a33c9d4ac361f15896cc3547085871521f97e6
Kiy Hong Tang <kiyhong@gmail.com>
2014-07-04 12:06:07 +0100

    Added test for returning a dime

--- spec/coin_changer_spec.rb
+++ spec/coin_changer_spec.rb
@@ -20,7 +20,27 @@ describe "CoinChanger" do
 		expect(coin.return_change(2)).to eq [1,1]
 	end
 
-	it "returns cahnge for 3 cents" do
+	it "returns change for 3 cents" do
 		expect(coin.return_change(3)).to eq [1,1,1]	
+	end
+
+	it "returns change for 5" do
+		expect(coin.return_change(5)).to eq [5]
+	end
+
+	it "returns change for 6" do
+		expect(coin.return_change(6)).to eq [5,1]
+	end
+
+	it "returns change for 7" do
+		expect(coin.return_change(7)).to eq [5,1,1]	
+	end
+
+	it "returns change for 10" do
+		expect(coin.return_change(10)).to eq [10]	
+	end
+
+	it "returns change for 20" do
+		expect(coin.return_change(20)).to eq [10,10]	
 	end
 end
