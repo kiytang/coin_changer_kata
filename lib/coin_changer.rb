9a2dcc883d19b42b7c98ba9a2e4d83cc55ff75b4
Kiy Hong Tang <kiyhong@gmail.com>
2014-07-04 12:06:52 +0100

    Refactored all if statements to while loops

--- lib/coin_changer.rb
+++ lib/coin_changer.rb
@@ -2,9 +2,20 @@ class CoinChanger
 
   def return_change(n)
     result = []
-      if n >= 1
-        n.times {result << 1 } 
+      while n >= 10
+        result << 10
+         n -= 10
       end
-    result
+
+      while n >= 5
+        result << 5
+        n -= 5
+      end
+
+      while n >= 1
+        result << 1
+        n -= 1
+      end
+      result
   end
 end
