d1c915e7cf162cdeb26b7be1fd8e8cd9a1734bad
Kiy Hong Tang <kiyhong@gmail.com>
2014-07-04 10:54:41 +0100

    Refactored test to remove duplication

--- spec/coin_changer_spec.rb
+++ spec/coin_changer_spec.rb
@@ -1,13 +1,13 @@
 require "coin_changer"
 
 describe "CoinChanger" do
+	let(:change) {CoinChanger.new}
+
 	it "can create a new instance of CoinChanger "do	
-		coin = CoinChanger.new
 		expect(change).not_to be_nil
 	end
 
 	it "returns [] for amount 0" do
-		coin = CoinChanger.new
 		expect(coin.return_change(0)).to eq []
 	end
 
